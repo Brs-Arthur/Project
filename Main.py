c2df0b84d2b54664f78400b003bcfe7ec4419d40
Brs-Arthur <65120528+Brs-Arthur@users.noreply.github.com>
2021-06-04 22:24:30 +0200

    CSV
    Main pour programme de travail cdv

--- Main.py
+++ Main.py
@@ -1,28 +1,37 @@
-from Rectangle import Rectangle
+import csv
+import random as rd
+import tkinter as tk
+from tkinter.filedialog import *
 
-running = True
-result = 'NON'
-while running:
-    list1 = []
-    list2 = []
-    try:
-        loop = int(input("Veuillez rentrer le nombre de pair necessaire"))
-        for i in range(loop):
-            for a in range(8):
-                if a < 4:
-                    list1.append(int(input("Veuillez rentrer pour le premier rectangle le nombre numéro {}".format(a + 1))))
-                else:
-                    list2.append(int(input("Veuillez rentrer pour le second rectangle le nombre numéro {}".format(a - 3))))
-                if a == 3:
-                    rect1 = Rectangle(list1[0], list1[1], list1[2], list1[3])
-                    rect1.addCoor()
-                if a == 7:
-                    rect2 = Rectangle(list2[0], list2[1], list2[2], list2[3])
-                    rect2.addCoor()
-            running = False
-            for coor in rect1.listCoor:
-                if coor in rect2.listCoor:
-                    result = "OUI"
-            print(result)
-    except:
-        break
+root = tk.Tk()
+root.title("Génerateur de Liste de Vocabulaire")
+root.geometry("300x200")
+
+
+def fileModify():
+    filename = askopenfilename(parent=root, title="Ouvrir votre document", initialdir="/",
+                               filetypes=[('csv files', '.csv'), ('all files', '.*')])
+    data = []
+    file = asksaveasfilename(title="Enregistrer sous...", filetypes=[("csv files", ".csv"), ("Tous", "*")], parent=root)
+    tk.Label(root, text="Votre fichier a bien été créé, vous le trouverez à compléter").pack(padx=10, pady=10)
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=";")
+        for row in spamreader:
+            data.append(row)
+
+    with open(file, 'w', newline='') as csvT:
+        spamwriter = csv.writer(csvT, delimiter=',')
+        for row in data:
+            if row[2] == '' or row[3] == '':
+                delete = rd.randint(0, 1)
+                row[delete] = ''
+            else:
+                for y in range(0, 3, 2):
+                    delete = rd.randint(0 + y, 1 + y)
+                    row[delete] = ''
+            spamwriter.writerow(row)
+
+
+button = tk.Button(root, text="Cliquer ici pour ouvir un fichier", command=fileModify)
+button.pack(pady=10)
+root.mainloop()
